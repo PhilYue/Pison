649ce7f03e1de786c60d73c19128a20ab31bc774
phil <phil.yue@foxmail.com>
2020-09-09 10:53:09 +0800

    first commit

---
+++ src/main/java/com/pi/pison/PisonApplication.java
@@ -0,0 +1,13 @@
+package com.pi.pison;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PisonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PisonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pi/pison/PisonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pi.pison;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PisonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
